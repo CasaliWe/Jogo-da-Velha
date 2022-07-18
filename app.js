a88bd49fb4f1048d8b1e3703216b1506185b0928
Weslei Casali <wesleicasali18@gmail.com>
2022-07-17 23:06:22 -0300

    att

--- app.js
+++ app.js
@@ -1,157 +1,184 @@
 document.getElementById('btn1').addEventListener('click', (e)=>{
        var btn = e.target
        var player = document.querySelector('#jogador > span').textContent
-       btn.textContent = player
-       if(document.querySelector('#jogador > span').textContent == 'X'){
-               document.querySelector('#jogador > span').textContent = 'O'
-       } else {
-               document.querySelector('#jogador > span').textContent = 'X'
-       }  
+       if(btn.textContent == ''){
+              btn.textContent = player
+              if(document.querySelector('#jogador > span').textContent == 'X'){
+                  document.querySelector('#jogador > span').textContent = 'O'
+              } else {
+                  document.querySelector('#jogador > span').textContent = 'X'
+              }  
+ 
+              if(document.getElementById('btn1').textContent == 'O'){
+                 document.getElementById('btn1').style.color = 'black'
+              }
+ 
+        verficiarV();
 
-       if(document.getElementById('btn1').textContent == 'O'){
-              document.getElementById('btn1').style.color = 'black'
-       }
+     }
 
-       verficiarV();
 })
 
 document.getElementById('btn2').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    }  
-
-    if(document.getElementById('btn2').textContent == 'O'){
-        document.getElementById('btn2').style.color = 'black'
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        }  
+    
+        if(document.getElementById('btn2').textContent == 'O'){
+            document.getElementById('btn2').style.color = 'black'
+        }
+    
+        verficiarV();
     }
 
-    verficiarV();
 })
 
 document.getElementById('btn3').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    }  
-
-    if(document.getElementById('btn3').textContent == 'O'){
-        document.getElementById('btn3').style.color = 'black'
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        }  
+    
+        if(document.getElementById('btn3').textContent == 'O'){
+            document.getElementById('btn3').style.color = 'black'
+        }
+    
+        verficiarV();
     }
 
-    verficiarV();
 })
+
 
 document.getElementById('btn4').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    }  
-
-    if(document.getElementById('btn4').textContent == 'O'){
-        document.getElementById('btn4').style.color = 'black'
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        }  
+    
+        if(document.getElementById('btn4').textContent == 'O'){
+            document.getElementById('btn4').style.color = 'black'
+        }
+    
+        verficiarV();
     }
 
-    verficiarV();
 })
 
 document.getElementById('btn5').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    } 
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        } 
+        
+        if(document.getElementById('btn5').textContent == 'O'){
+            document.getElementById('btn5').style.color = 'black'
+        }
     
-    if(document.getElementById('btn5').textContent == 'O'){
-        document.getElementById('btn5').style.color = 'black'
+        verficiarV();
     }
 
-    verficiarV();
 })
 
 document.getElementById('btn6').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    } 
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        } 
+        
+        if(document.getElementById('btn6').textContent == 'O'){
+            document.getElementById('btn6').style.color = 'black'
+        }
     
-    if(document.getElementById('btn6').textContent == 'O'){
-        document.getElementById('btn6').style.color = 'black'
+        verficiarV();
     }
 
-    verficiarV();
 })
 
 document.getElementById('btn7').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    }  
-
-    if(document.getElementById('btn7').textContent == 'O'){
-        document.getElementById('btn7').style.color = 'black'
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        }  
+    
+        if(document.getElementById('btn7').textContent == 'O'){
+            document.getElementById('btn7').style.color = 'black'
+        }
+    
+        verficiarV();
     }
 
-    verficiarV();
 })
 
 document.getElementById('btn8').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    }  
-
-    if(document.getElementById('btn8').textContent == 'O'){
-        document.getElementById('btn8').style.color = 'black'
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        }  
+    
+        if(document.getElementById('btn8').textContent == 'O'){
+            document.getElementById('btn8').style.color = 'black'
+        }
+    
+        verficiarV();
     }
 
-    verficiarV();
 })
 
 document.getElementById('btn9').addEventListener('click', (e)=>{
     var btn = e.target
     var player = document.querySelector('#jogador > span').textContent
-    btn.textContent = player
-    if(document.querySelector('#jogador > span').textContent == 'X'){
-            document.querySelector('#jogador > span').textContent = 'O'
-    } else {
-            document.querySelector('#jogador > span').textContent = 'X'
-    }  
-
-    if(document.getElementById('btn9').textContent == 'O'){
-        document.getElementById('btn9').style.color = 'black'
+    if(btn.textContent == ''){
+        btn.textContent = player
+        if(document.querySelector('#jogador > span').textContent == 'X'){
+                document.querySelector('#jogador > span').textContent = 'O'
+        } else {
+                document.querySelector('#jogador > span').textContent = 'X'
+        }  
+    
+        if(document.getElementById('btn9').textContent == 'O'){
+            document.getElementById('btn9').style.color = 'black'
+        }
+    
+        verficiarV();
     }
 
-    verficiarV();
 })
-
-
 
 
 
